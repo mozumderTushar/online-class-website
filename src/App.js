86d2b8d5a143768b8ef6d4dcc1156820d349d02a
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-08-27 03:23:36 +0600

    course and cart item added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import fakeData from './fakeData/course.js'
+import CourseItem from './components/Courses/CourseItem';
+import Cart from './components/Cart/Cart';
 
 function App() {
+
+  // get fakeData
+  const [courses, setCourses] = useState(fakeData)
+  console.log(fakeData);
+
+  //set cart
+  const [cart, setCart] = useState([])
+
+  //click handler
+  const handleAddCart = (item) => {
+    console.log("click",item);
+    const newCart = [...cart, item]
+    setCart(newCart)
+  }
+
+  //total price
+  const totalPrice = (cart.reduce((sum, item) => sum + item.price,0)).toFixed(2)
+
   return (
-    <div >
-       <h3>This is online </h3>
+    <div className="container-fluid">
+       <h2 className="text-center mt-5">Online Course</h2>
+       <div className="row course-items">
+         <div className="col-md-9 row border-right mt-3">
+           {
+             courses.map(course => <CourseItem course={course} key={course.id} handleAddCart={handleAddCart}></CourseItem>)
+           }
+         </div>
+         <div className="col-md-3 m-2">
+          <h2 className="text-center">Cart {cart.length}</h2>
+          <ul className="list-group">
+              {
+                cart.map(item => <Cart item={item}></Cart>)
+              }
+          </ul>
+          <button className="btn btn-primary btn-block">Checkout <span className="badge badge-light">{totalPrice}</span></button>
+         </div>
+       </div>
     </div>
   );
 }
---
+++ src/components/Courses/CourseItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './CourseItem.css'
+
+const CourseItem = (props) => {
+
+    const {img, title, instructor, level, price} = props.course;
+    return (
+        <div className="col-md-4">
+           <div className="single-item my-4">
+            <div className="card">
+                    <img src={img} alt="" className="card-img-top"/>
+                <div className="card-body">
+                    <div className="card-title"><h3>{title}</h3></div>
+                    <p>{instructor}</p>
+                    <p><small>{level}</small></p>
+                    <h4 className="price">${price}</h4>
+                    <button className="btn btn-sm btn-primary" onClick={()=>props.handleAddCart(props.course)}>Enroll Now</button>
+                </div>
+         </div>
+           </div>
+        </div>
+    );
+};
+
+export default CourseItem;
