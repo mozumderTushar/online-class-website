466eebae51699e8bab4c9aa6d1c793b223383b9c
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-08-27 19:33:12 +0600

    rating added

--- src/components/CourseItem/CourseItem.js
+++ src/components/CourseItem/CourseItem.js
@@ -13,6 +13,13 @@ const CourseItem = (props) => {
                     <div className="card-title"><h3>{title}</h3></div>
                     <p>{instructor}</p>
                     <p><small>{level}</small></p>
+                    <div className="rating">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                    </div>
                     <h4 className="price">${price}</h4>
                     <button className="btn btn-sm btn-primary enrollBtn" onClick={()=>props.handleAddCart(props.course)}>Enroll Now</button>
                 </div>
